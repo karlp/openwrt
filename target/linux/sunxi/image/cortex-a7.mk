a63d003f2d1b1e1821c9e2bb362ad7ea454d64b8
Karl Palsson <karlp@tweak.net.au>
2018-12-09 21:14:39 +0000

    karl: add iotbox dts and build
    The IoT Box adds, (amongst others) an Ethernet port and two USB host
    ports.
    
    Ref: http://wiki.friendlyarm.com/wiki/index.php/NanoPi_Duo2_IoT-Box
    
    USB ports on baseboard don't work?!

--- target/linux/sunxi/image/cortex-a7.mk
+++ target/linux/sunxi/image/cortex-a7.mk
@@ -134,8 +134,16 @@ define Device/sun8i-h3-nanopi-duo2
   SUPPORTED_DEVICES:=friendlyarm,nanopi-duo2
   SUNXI_DTS:=sun8i-h3-nanopi-duo2
 endef
-
 TARGET_DEVICES += sun8i-h3-nanopi-duo2
+
+define Device/sun8i-h3-nanopi-duo2-iotbox
+  DEVICE_TITLE:=FriendlyARM NanoPi Duo2 IoT Box
+  DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-brcmfmac brcmfmac-firmware-43430-sdio \
+	 wpad-basic
+  SUPPORTED_DEVICES:=friendlyarm,nanopi-duo2-iotbox
+  SUNXI_DTS:=sun8i-h3-nanopi-duo2-iotbox
+endef
+TARGET_DEVICES += sun8i-h3-nanopi-duo2-iotbox
 
 define Device/sun8i-h3-nanopi-m1-plus
   DEVICE_TITLE:=FriendlyArm NanoPi M1 Plus
